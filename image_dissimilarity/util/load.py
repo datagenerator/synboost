706bd384b92980688a8d485724f26cd55a15fdeb
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2022-01-02 11:59:45 +0530

    load ckp direct

--- image_dissimilarity/util/load.py
+++ image_dissimilarity/util/load.py
@@ -14,3 +14,10 @@ def load_ckp(wandb_base_path, name, epoch):
     checkpoint = torch.load(load_path)
 
     return checkpoint
+
+def load_ckp_direct(wandb_base_path, name, epoch):      
+
+    # load check point
+    load_path = "/content/drive/MyDrive/driving_uncertainty/models/image-dissimilarity/best_net_baseline_void_prior_spadedecoder_mult_3.pth"
+    checkpoint = torch.load(load_path)
+    return checkpoint
